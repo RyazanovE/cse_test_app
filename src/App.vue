8dcc5cb9280522f9ac0137994bc5d708d6c674d2
RyazanovE <sidewipe@yandex.ru>
2022-09-10 12:51:57 +0300

    user_page_ready

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="grid grid-cols-6 min-h-screen">
     <sidebar class="col-span-1" />
-    <div class="col-span-5  flex flex-col ">
-		<app-header class=" "/>
-		<router-view class=""/>
+    <div class="col-span-5 flex flex-col">
+      <app-header class=" " />
+        <router-view />
     </div>
   </div>
 </template>
 
 <script>
-
 import Sidebar from "@/components/sidebar/Sidebar.vue";
-import AppHeader from "@/components/app-header/AppHeader.vue"
+import AppHeader from "@/components/app-header/AppHeader.vue";
 export default {
   components: { Sidebar, AppHeader },
 };
