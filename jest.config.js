46c2afb16c9e778ee11ad4237766baa5cdf35a32
RyazanovE <sidewipe@yandex.ru>
2022-09-10 15:35:05 +0300

    environment setup

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	"testEnvironment": "jsdom",
+	testEnvironmentOptions: {
+		customExportConditions: ["node", "node-addons"],
+	},
+	"moduleFileExtensions": ["js", "json", "vue"],
+	"transform": {
+		"^.+\\.js$": "babel-jest",
+		"^.+\\.vue$": "@vue/vue3-jest"
+	},
+	"moduleNameMapper": {
+		"^@/(.*)$": "<rootDir>/src/$1"
+	}
+}
