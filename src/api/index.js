6041a44193dc63e1c8ed042f6eca9140d0d5ae6e
RyazanovE <sidewipe@yandex.ru>
2022-09-10 23:48:45 +0300

    github_token_remove hot_fix

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,6 @@ const api = axios.create({
 	baseURL: 'https://api.github.com/',
 	headers: {
 		"Accept": "application/vnd.github+json",
-		"Authorization": "Bearer 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"
 	}
 });
 
